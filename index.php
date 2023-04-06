12506c46d0616b7746c94b9f1867ea56f1d46288
Adriano B. Cerutti <adrianocerutti@gmail.com>
2023-04-05 22:29:52 -0300

    Adicionada a classe Mensagem e os métodos de mensagens

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
 <?php
 
 require_once 'sistema/configuracao.php';
 include_once 'Helpers.php';
+include './sistema/Nucleo/Mensagem.php';
 
-$cpf = '004.348.491-38';
+$msg = new Mensagem();
+echo $msg->erro('Mensagem de erro')->renderizar();
+echo '<hr>';
+var_dump($msg);
 
-var_dump(validarCpf($cpf));
-
-// echo $limpaNumero = preg_replace('/[^0-9]/', '', $cpf);
-
-// Parei na aula 39
+// Parei na aula 43
