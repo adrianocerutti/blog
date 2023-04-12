019f18eb6f29d3fff1d7ca724e36920794fb03b2
Adriano B. Cerutti <adrianocerutti@gmail.com>
2023-04-12 14:40:17 -0300

    Implementado Twig Template

--- sistema/Controlador/SiteControlador.php
+++ sistema/Controlador/SiteControlador.php
@@ -2,15 +2,28 @@
 
 namespace sistema\Controlador;
 
-class SiteControlador
+use sistema\Nucleo\Controlador;
+
+class SiteControlador extends Controlador
 {
+    public function __construct()
+    {
+        parent::__construct('templates/site/views');
+    }
+
     public function index(): void
     {
-       echo 'página index';
+       echo $this->template->renderizar('index.html', [
+        'titulo' => 'teste de título',
+        'subtitulo' => 'teste de subtítulo'
+       ]);
     }
 
     public function sobre(): void
     {
-        echo 'página sobre';
+        echo $this->template->renderizar('sobre.html', [
+            'titulo' => 'teste de título sobre',
+            'subtitulo' => 'teste de subtítulo sobre'
+           ]);
     }
 }
